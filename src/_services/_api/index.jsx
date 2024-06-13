22a907494a4c72de42ba3886e365b421a9a872d3
Yasin Zahiri <yasin.zahiri10@gmail.com>
2024-06-13 16:45:47 +0330

    feat(_services/_api): added error handling to service api functions

--- src/_services/_api/index.jsx
+++ src/_services/_api/index.jsx
@@ -1,21 +1,38 @@
+import { toast } from "react-toastify";
 import { axiosInstance } from "../../_utils/axiosInstance";
 
 export const getTodos = async () => {
-  const res = await axiosInstance.get(`todos`);
-  return res.data;
+  try {
+    const res = await axiosInstance.get(`todos`);
+    return res.data;
+  } catch (error) {
+    toast.error(`Error: failed to get your todo list`);
+  }
 };
 
 export const addNewTodo = async (data) => {
-  const res = await axiosInstance.post("todos", data);
-  return res.data;
+  try {
+    const res = await axiosInstance.post("todos", data);
+    return res.data;
+  } catch (error) {
+    toast.error(`Error: failed to add new todo item`);
+  }
 };
 
 export const editTodo = async (data) => {
-  const res = await axiosInstance.put(`todos/${data.id}`, data);
-  return res.data;
+  try {
+    const res = await axiosInstance.put(`todos/${data.id}`, data);
+    return res.data;
+  } catch (error) {
+    toast.error(`Error: failed to edit your todo item`);
+  }
 };
 
 export const removeTodo = async (todoId) => {
-  const res = await axiosInstance.delete(`todos/${todoId}`);
-  return res.data;
+  try {
+    const res = await axiosInstance.delete(`todos/${todoId}`);
+    return res.data;
+  } catch (error) {
+    toast.error(`Error: failed to remove your todo item`);
+  }
 };
