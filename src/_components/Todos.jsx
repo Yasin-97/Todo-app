cd12a5a51d5de50bc260919c2579754da351301b
Yasin Zahiri <yasin.zahiri10@gmail.com>
2024-06-14 15:54:28 +0330

    feat(todos): added functionality to toggle and remove tood item

--- src/_components/Todos.jsx
+++ src/_components/Todos.jsx
@@ -1,6 +1,10 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { getTodosAsync } from "../_redux/todoSlice";
+import {
+  editTodoAsync,
+  getTodosAsync,
+  removeTodoAsync,
+} from "../_redux/todoSlice";
 import SingleTodo from "./SingleTodo";
 
 const Todos = () => {
@@ -11,10 +15,28 @@ const Todos = () => {
     dispatch(getTodosAsync());
   }, []);
 
+  const handleRemove = (id) => {
+    dispatch(removeTodoAsync(id));
+  };
+
+  const toggleComplete = (newTodoData) => {
+    dispatch(editTodoAsync(newTodoData));
+  };
+
   return (
     <div className="todos-container">
+      {!todos?.data?.length && (
+        <p className="empty-todo-text">You got nothing to do for now !</p>
+      )}
       {todos?.data?.map((todo) => (
-        <SingleTodo key={todo.id} data={todo} />
+        <SingleTodo
+          key={todo.id}
+          data={todo}
+          onRemove={() => handleRemove(todo.id)}
+          onToggleComplete={() =>
+            toggleComplete({ id: todo.id, completed: !todo.completed })
+          }
+        />
       ))}
     </div>
   );
